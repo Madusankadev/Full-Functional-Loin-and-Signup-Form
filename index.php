d188f439c4f44a8109700d71b9e603614d8c7865
Ishara Madusanka <isaharamadusanka.dev@gmail.com>
2024-02-07 22:16:58 +0530

    Sign Up frontend view Developed

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./res/css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="signupBox">
+            <img src="./res/icon/user.png" alt="user" width="100" height="100">
+
+            <label for="userName">User Name</label>
+            <input type="text" id="userName" class="userName">
+
+            <label for="email">Email</label>
+            <input type="email" id="email" class="email">
+
+            <label for="pswd">Password</label>
+            <input type="password" id="pswd">
+
+            <div class="rememberMe">
+                <input type="checkbox" id="rmeCheck">
+                <label for="rmeCheck">Remember Me</label>
+            </div>
+
+            <button class="btn">Sign Up</button>
+
+            <div class="signupLinks">
+                <a href="/">Forgot Password</a>
+                <a href="">Already have an account</a>
+            </div>
+
+
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
